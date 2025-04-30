a5b323066092fce6de78b56e2c19146bb4444bb0
mahmud1628 <2105120@ugrad.cse.buet.ac.bd>
2025-04-30 15:38:00 +0600

    Unzipping done

---
+++ Assignment 1/Workspace/organize.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+PATH_TO_SUBMISSION_FOLDER=$1
+# PATH_TO_TARGET_FOLDER=$2
+# PATH_TO_TEST_FOLDER=$3
+# PATH_TO_ANSWER_FOLDER=$4
+
+#unzip all the files in the submission folder
+for zipped_file in "$PATH_TO_SUBMISSION_FOLDER"/*
+do
+    # echo "Processing $zipped_file"
+    student_id=$(basename "$zipped_file" | grep -oE '_[0-9]{7}\.zip$' | grep -oE '[0-9]{7}')
+    # echo "Student ID: $student_id"
+    # Unzip the file
+    unzip "$zipped_file" -d "$PATH_TO_SUBMISSION_FOLDER"
+    rm "$zipped_file"
+done
+
+
+
+
